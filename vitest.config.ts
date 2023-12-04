62b0ce839b47e333e4290958245dac3adbac7f8b
abdulelhakim68@gmail.com <elhakimdev>
2023-12-04 20:39:17 +0700

    include in-source testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vitest/config';
 // "https://vitest.dev/guide/#configuring-vitest"
 export default defineConfig({
+  test: {
+    includeSource: ['src/**/*.{js,ts}']
+  }
 })
